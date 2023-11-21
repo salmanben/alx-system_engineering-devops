4a8540df8a00702d6f85f9ad27003e98ba54d232
zjaad <zjaadani@gmail.com>
2023-11-21 15:53:04 -0800

    updating Place: (models/place.py), User: (models/user.py) , City: (models/city.py) -- Task N8

--- models/user.py
+++ models/user.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from sqlalchemy.ext.declarative import declarative_base
-from models.base_model import BaseModel, Base
-#   from models.base_model import 
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, Integer, String
-#from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
+from models.review import Review
+from models.place import Place
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
+    """This class defines a user
+            email : Email
+            password: Password
+            first_name: First name
+            last_name: Last name
+            places = Places
+    """
     __tablename__ = "users"
-    email = Column(String(128), nullable = False)
-    password = Column(String(128), nullable = False)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    places = relationship("Place", cascade='all, delete, delete-orphan', backref="user")
