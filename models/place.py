18a1b5d5e9fb5290c233a1e5d8f34b966c2cec10
zjaad <zjaadani@gmail.com>
2023-11-22 15:06:41 -0800

    working on - Amenity task number10

--- models/place.py
+++ models/place.py
@@ -6,7 +6,7 @@ from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
 import models
 from os import getenv
-
+import shlex
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -24,6 +24,31 @@ class Place(BaseModel, Base):
     longitude = Column(Float)
     amenity_ids = []
 
-    #if getenv 
-        # reviews = relationship("Review", backref="place", cascade="all, delete-orphan")
-        #
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+         place_amenity = Table("place_amenity", Base.metadata,
+                              Column("place_id", String(60), ForeignKey("places.id"),
+                                     primary_key=True, nullable=False),
+                              Column("amenity_id", String(60), ForeignKey("amenities.id"),
+                                     primary_key=True, nullable=False))
+
+        reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="place")
+        amenities = relationship("Amenity", secondary=place_amenity,
+                                 viewonly=False,
+                                 back_populates="place_amenities")
+    else:
+        @property
+        def reviews(self):
+            """ Returns list of reviews.id """
+            sd = models.storage.all()
+            li = []  # List to store review instances
+            r = []  # Result list
+            for k in sd:
+                review = k.replace('.', ' ')
+                review = shlex.split(review)
+                if review[0] == 'Review':
+                    li.append(sd[k])
+            for o in li:
+                if o.place_id == self.id:
+                    r.append(o)
+            return r
+        
