35c2b1854b95833c2ed919cc0641fd39c0d80310
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2023-11-23 00:46:04 +0100

    Update place.py

--- models/place.py
+++ models/place.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from sqlalchemy.orm import relationship
-import models
-import shlex
+from models.base_model import BaseModel, Base
 
 class Place(BaseModel, Base):
-    """ A place to stay """
-    __tablename__ = "places"
-    id = Column(String(60), primary_key=True, nullable=False)
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """A place to stay"""
+    __tablename__ = 'places'
+
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -22,33 +18,11 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float)
     longitude = Column(Float)
-    amenity_ids = []
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-         place_amenity = Table("place_amenity", Base.metadata,
-                              Column("place_id", String(60), ForeignKey("places.id"),
-                                     primary_key=True, nullable=False),
-                              Column("amenity_id", String(60), ForeignKey("amenities.id"),
-                                     primary_key=True, nullable=False))
+    # Define relationships
+    # Assuming City and User models exist with appropriate relationships
+    city = relationship("City")
+    user = relationship("User")
 
-        reviews = relationship("Review", cascade='all, delete, delete-orphan', backref="place")
-        amenities = relationship("Amenity", secondary=place_amenity,
-                                 viewonly=False,
-                                 back_populates="place_amenities")
-    else:
-        @property
-        def reviews(self):
-            """ Returns list of reviews.id """
-            sd = models.storage.all()
-            li = []  # List to store review instances
-            r = []  # Result list
-            for k in sd:
-                review = k.replace('.', ' ')
-                review = shlex.split(review)
-                if review[0] == 'Review':
-                    li.append(sd[k])
-            for o in li:
-                if o.place_id == self.id:
-                    r.append(o)
-            return r
-        
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
