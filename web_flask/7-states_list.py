07db1ba53a9c2ecc6feec46128cff2936ebc5116
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-01-01 21:34:36 +0100

    Update 7-states_list.py

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,23 @@
 #!/usr/bin/python3
-"""Importing Flask to run the web app"""
+"""Starts a Flask web application"""
 from flask import Flask, render_template
-from models import storage
 from models.state import State
+from models import storage
 
 
 app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def display_states_list():
-    """Render states_list.html page to display the created State instances"""
-    states = storage.all(State)
-    return render_template('7-states_list.html', states=states)
+def display_states():
+    """Render states list in html page"""
+    states = storage.all()
+    return render_template('7-state_list.html', states=states)
 
 
 @app.teardown_appcontext
-def close_storage(exception):
-    """Method to remove the current SQLAlchemy Session"""
+def teardown(self):
+    """Remove current SQLalchemy Session"""
     storage.close()
 
 
