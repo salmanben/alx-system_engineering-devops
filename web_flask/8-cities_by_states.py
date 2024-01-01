74bc3be79bc41440ce82fe8db7f9acdbc3dbbce7
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-01-01 21:37:59 +0100

    Create 8-cities_by_states.py

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""importing Flask to run the web app"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close(self):
+    """ Close the session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def d_cities_by_states():
+    """Displays a html page contains states and cities"""
+    states = storage.all(State)
+    cities = storage.all(City)
+    return render_template(
+            '8-cities_by_states.html', states=states, cities=cities)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
