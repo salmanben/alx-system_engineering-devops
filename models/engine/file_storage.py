8dfec8d0b27318ad51f2ed3afefab4042934f402
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-01-01 21:19:11 +0100

    Update file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,3 +56,6 @@ class FileStorage:
             key_to_delete = "{}.{}".format(obj.__class__.__name__, obj.id)
             if key_to_delete in FileStorage.__objects:
                 del FileStorage.__objects[key_to_delete]
+    def close(self):
+       """Call the reload method"""
+       self.reload()
