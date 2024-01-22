da4928d69f22bfc05d4d11f2e2cc8aaf2e18ca28
salmanben <salmanbenomar250@gmail.com>
2024-01-22 01:51:20 +0000

    Add Puppet file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Automated fixing is employed to identify the reason why Apache is returning a 500 error
+
+exec { 'Fix wordpress site':
+  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  provider => shell,
+}
