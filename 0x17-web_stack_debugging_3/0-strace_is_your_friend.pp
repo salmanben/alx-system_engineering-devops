61615cc2cecf8b7359aa1f22f5e4ab98827c7c89
SALMAN BEN OMAR <salmanbenomar250@gmail.com>
2024-02-07 03:44:43 +0100

    Delete 0x17-web_stack_debugging_3 directory

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++
@@ -1,6 +0,0 @@
-# Automated fixing is employed to identify the reason why Apache is returning a 500 error
-
-exec { 'Fix wordpress site':
-  command  => 'sudo sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => shell,
-}
